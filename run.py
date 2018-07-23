5f469e11524ff2c534587b71b814da5b9311b081
Tom McNally <tom_mcnally17@hotmail.co.uk>
2018-07-23 16:39:35 +0000

    added routing for edit_book, update_book and delete_book also for book_record

--- run.py
+++ run.py
@@ -17,18 +17,18 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     
-    # display homepage and content
+    # Display homepage and content
     _books = mongo.db.books.find()
     books = [book for book in _books]
     
-    # most viewed books to display
+    # Most viewed books to display
     most_viewed_books = mongo.db.books.find(
         { "$query": {},
             "$orderby": { "views" : -1 }}).limit(5)
             
     most_viewed = [book for book in most_viewed_books]
     
-    # random featured book
+    # Random featured book
     i = randint(0, (len(books) - 1)) 
     featured_book = books[i]
     featured = {
@@ -38,19 +38,61 @@ def index():
         "ISBN": re.sub(r'[^\w.]', '', featured_book["ISBN"]) 
     }
     
-    # list of genres to filter by
+    # List of genres to filter by
     _genres = get_genres()
     
     return render_template("index.html",
                             featured=featured,
                             most_viewed=most_viewed,
                             genres=_genres)
+                            
+@app.route("/search")
+def search(search_for):
+    
+    mongo.db.books.createIndex({ "$**": "text" })
+    search_results = mongo.db.books.find({ "$text": { "$search": search_for }})
+    
+    return search_results
 
+
+ ### Book CRUD Operations
+ 
+@app.route("/book_record/<book_id>")
+def book_record(book_id):
+    
+    # Displays the book record with Edit and Delete opertations
+    
+    book_record = find_book(book_id)
+    book = {
+        "title": book_record["title"],
+        "author": ",".join(book_record["author"]),
+        "genre": ",".join(book_record["genre"]),
+        "blurb": book_record["blurb"],
+        "publisher": ",".join(book_record["publisher"]),
+        "ISBN": book_record["ISBN"] 
+    }
+    
+    return render_template("book_record.html", book=book)
+
+def update_reviews(book_id):
+    
+    # Update reviews and rating
+    
+    mongo.db.books.update({"_id": ObjectId(book_id)},
+    { "$push":
+        {
+            "reviews": {
+                "name": "Me",
+                "review": "This is a test"
+            },
+            "rating": 1
+        }
+    })
 
 @app.route("/add_book")
 def add_book():
     
-    # go to add book form
+    # Go to add book
     return render_template("add_book.html") 
     
 @app.route("/insert_book", methods = ["POST"])
@@ -71,6 +113,41 @@ def insert_book():
         })
     
     return redirect(url_for("add_book"))
+    
+@app.route("/edit_book/<book_id>")
+def edit_book(book_id):
+    
+    # Go to edit book
+    return render_template("edit_book.html")
+    
+@app.route("/update_book", methods = ["POST"])
+def update_book(book_id):
+    
+    # Update book
+    
+    mongo.db.books.update(
+        {"_id": ObjectId(book_id)},
+        { "$set": 
+            {
+                "title": "Test Book 1",
+                "blurb": "This is a test book, updated",
+                "ISBN": "None"
+            }
+        })
+    mongo.db.books.update(
+        {"_id": ObjectId(book_id)},
+        { "$addToSet":
+            {
+                "genre": "Test",
+                "author": "No one",
+                "publisher": "Me"
+            }
+        })
+        
+@app.route("/delete_book")
+def delete_book(book_id):
+    
+    mongo.db.books.remove({"_id": ObjectId(book_id)})
 
     
 def find_all_books():
@@ -80,12 +157,17 @@ def find_all_books():
     
     return books
     
-def search(search_for):
+def find_book(book_id):
     
-    mongo.db.books.createIndex({ "$**": "text" })
-    search_results = mongo.db.books.find({ "$text": { "$search": search_for }})
+    book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
     
-    return search_results
+    return book
+    
+def find_last_inserted():
+    
+    last_book = mongo.db.books.find_one({ "$query": {}, "$orderby": { "_id" : -1 }}, { "_id": 0})
+    
+    return last_book
 
 def get_genres():
     
@@ -110,13 +192,27 @@ def get_authors():
             authors.append(author)
     
     return authors
-
+    
+    
+    ### Test CRUD Operations
+    
+def find_test_book(book_id):
+    
+    book = mongo.db.test.find_one({"_id": ObjectId(book_id)})
+    
+    return book
+    
+def find_last_test():
+    
+    last_book = mongo.db.test.find_one({ "$query": {}, "$orderby": { "_id" : -1 }}, { "_id": 0})
+    
+    return last_book
     
 def insert_test_book():
 
     # Insert test book record
     
-    mongo.db.books.insert_one({
+    mongo.db.test.insert_one({
             "title": "Test Book 2",
             "author": ["Me"],
             "genre": ["Test 1"],
@@ -127,24 +223,12 @@ def insert_test_book():
             "reviews": [],
             "ratings": []
         })
-
-def find_book(book_id):
-    
-    book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
-    
-    return book
-    
-def find_last_inserted():
-    
-    last_book = mongo.db.books.find_one({ "$query": {}, "$orderby": { "_id" : -1 }}, { "_id": 0})
-    
-    return last_book
-    
-def update_book(book_id):
+        
+def update_test_book(book_id):
     
     # Update book
     
-    mongo.db.books.update(
+    mongo.db.test.update(
         {"_id": ObjectId(book_id)},
         { "$set": 
             {
@@ -163,11 +247,11 @@ def update_book(book_id):
             }
         })
         
-def update_reviews(book_id):
+def update_test_reviews(book_id):
     
     # Update reviews and rating
     
-    mongo.db.books.update({"_id": ObjectId(book_id)},
+    mongo.db.test.update({"_id": ObjectId(book_id)},
     { "$push":
         {
             "reviews": {
@@ -178,10 +262,8 @@ def update_reviews(book_id):
         }
     })
     
-def delete_book(book_id):
-    
-    mongo.db.books.remove({"_id": ObjectId(book_id)})
 
+    
 
 if __name__ == "__main__":
     app.run(host= os.environ.get("IP"),
