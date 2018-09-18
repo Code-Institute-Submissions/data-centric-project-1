c9bc17d229c06e37f5fa75311de5bbf5d09081fc
Tom McNally <tom_mcnally17@hotmail.co.uk>
2018-09-18 20:52:35 +0000

    added text search test and testing documentation

---
+++ tests/test_run.py
@@ -0,0 +1,132 @@
+from bson.objectid import ObjectId
+import unittest
+import run
+
+
+class test_books(unittest.TestCase):
+    
+    # Test Suite
+    
+    def test_find_book(self):
+        
+        # Find book record by ID
+        
+        book = run.find_test_book("5b55febe207f2b4319b8c010")
+        
+        test_book = {
+            "_id": ObjectId("5b55febe207f2b4319b8c010"),
+            "title": "Test Book",
+            "author": ["No one"],
+            "genre": ["Test"],
+            "blurb": "This is a test book",
+            "publisher": ["Me"],
+            "ISBN": "None",
+            "views": 0,
+            "reviews": [],
+            "ratings": []
+        }
+        
+        self.assertEqual(test_book, book)
+        
+    def test_update_book(self):
+        
+        # Update book record by ID
+        
+        run.update_test_book("5b56efbe207f2b5a522cd07d")
+        book = run.find_test_book("5b56efbe207f2b5a522cd07d")
+        
+        test_book = {
+            "_id": ObjectId("5b56efbe207f2b5a522cd07d"),
+            "title": "Test Book 1",
+            "author": ["Me", "No one"],
+            "genre": ["Test 1"],
+            "blurb": "This is a test book, updated",
+            "publisher": ["No one", "Me"],
+            "ISBN": "None",
+            "views": 0,
+            "reviews": [],
+            "ratings": []
+        }
+        
+        
+        self.assertEqual(test_book, book)
+        
+    def test_get_genres(self):
+        
+        # Test get genre list
+        
+        _genres = run.get_genres()
+        test_genre = "History"
+        
+        self.assertIn(test_genre, _genres)
+        
+    def test_get_authors(self):
+        
+        # Test get author list 
+        
+        _authors = run.get_authors()
+        test_author = "Robyn Young"
+        
+        self.assertIn(test_author, _authors)
+        
+    def test_insert_book(self):
+        
+        # Test that the book inserted correctly
+        
+        run.insert_test_book()
+        
+        last_book = run.find_last_test()
+        
+        test_book = {
+            "title": "Test Book 2",
+            "author": ["Me"],
+            "genre": ["Test 1"],
+            "blurb": "This is a test book",
+            "publisher": ["No one"],
+            "ISBN": "None",
+            "views": 0,
+            "reviews": [],
+            "ratings": []
+        }
+        
+        self.assertEqual(test_book, last_book)
+        
+    def test_update_reviews(self):
+        
+        # Test for updating reviews and rating by ID
+        
+        run.update_test_reviews("5b55ff10207f2b4522d99830")
+        book = run.find_test_book("5b55ff10207f2b4522d99830")
+        
+        book_reivew = book["reviews"]
+        
+        test_review = {
+                "name": "Me",
+                "review": "This is a test"
+                }
+        
+        self.assertIn(test_review, book_reivew)
+        
+    def test_text_search(self):
+        
+        # Test to check text search returns correct results
+        
+        books = run.text_search_test("Book 1")
+        
+        test_book = {
+            "_id": ObjectId("5b56efbe207f2b5a522cd07d"),
+            "title": "Test Book 1",
+            "author": ["Me", "No one"],
+            "genre": ["Test 1"],
+            "blurb": "This is a test book, updated",
+            "publisher": ["No one", "Me"],
+            "ISBN": "None",
+            "views": 0,
+            "reviews": [],
+            "ratings": []
+        }
+        
+        self.assertIn(test_book, books)
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/user_test.md
@@ -0,0 +1,55 @@
+### User Testing
+
+As I deployed a new feature, I did manual tests to check the UI and UX worked as expected.
+Below is a list of features and the tests I carried out:
+
+- Add Book Form
+    - Navbar Link goes to "Add Book" page
+    - Try to submit empty form and required inputs stops submit
+    - Try submit more than the main author or one genre and validation highlights relevant help text
+    - Enter all inputs and submit redirects user to the "Book Record" they just added successfully
+    - "Clear" button clears form
+
+- Edit Book Form
+    - "Edit" button in "Book Record" goes to "Edit Book" page
+    - Form is prepopulated with Book details from DB
+    - Try submit form with an empty input and required input stops submit
+    - Try submit more than the main author or one genre and validation highlights relevant help text
+    - Enter edit any inputs and submit redirects user to the "Book Record" they just edited successfully
+
+- Delete Book
+    - "Delete?" button in "Book Record" opens a confirmation modal
+    - "Close" button closes modal
+    - "Delete" button deletes relevant book from DB and redirects user to Homepage
+
+- Full Text Search
+    - Try to submit with empty input and required input stops submit
+    - Search word or pharse redirects to results page with results of the search term
+    - Quick Search buttons redirects to results page with results of the search term
+    - Results can be sorted and filtered, by genre and author, or clear filters to return to search term
+    - Sorting and Filtering can be done in any order and if not results are found will display "No results Found"
+
+- View Book
+    - "View Book" button redirects to "Book Record" page with relevant book id
+    - "Book Record" page displays relevant details for book selected
+    - Ratings Chart displays relevant ratings data for book
+
+- Review Form
+    - "Write a review" button displays Review Form on "Book Record" page
+    - Try to submit empty form and required inputs stops submit
+    - Enter all inputs and submit adds a review to "Book Record" page for user to see
+    - Successful reviews also add to rating data which updates the rating chart
+
+- Recommend Form
+    - Try to submit empty form and required input stops submit
+    - Enter a username that does not exist in users DB will display "This username does not exist. Please try again."
+    - Enter a username that is in users DB will display "This has been Successfully Recommended"
+
+- My Books Login
+    - Try to submit empty Register form and required input stops submit
+    - Try to submit empty Login form and required input stops submit
+    - Enter input for Register form and submit redirects to "My Books" for username
+    - Enter input for Register form that already exists and submit displays "This username is not available. Please try again."
+    - Enter input for Login form and submit redirects to "My Books" for username
+    - Enter input for Login form that doesn't exist and submit displays "This username does not exist. Please try again or Register."
+
