83af029d225e51c12ab6d769d8734944dacd7d7a
Tom McNally <tom_mcnally17@hotmail.co.uk>
2018-09-18 20:50:31 +0000

    added some custom form validation and updated styles for validation

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -30,4 +30,29 @@ $( document ).ready(function() {
         $(".show-searches").hide();
     });
     
+    $("#add-book").submit(function(event) {
+        
+		var form = $(this);			
+		var author = form.find("input[name='author']");
+		var genre = form.find("input[name='genre']");
+
+		var authorVal = author.val();
+		var genreVal = genre.val();
+		var pattern_1 = /^([a-zA-Z]+)?([\s])?([a-zA-Z]+[-]?[a-zA-Z]+)?([\s])?$/;
+		var pattern_2 = /^([a-zA-Z]+)?([-])?([a-zA-Z]+)?([\s])?$/;
+
+
+		if (!pattern_1.test(authorVal)) {
+		    event.preventDefault();
+		    $("#author-help").addClass("error");
+		} else if (!pattern_2.test(genreVal)) {
+			event.preventDefault();
+			$("#author-help").removeClass("error");
+			$("#genre-help").addClass("error");
+		} else {
+		    $("#author-help").removeClass("error");
+			$("#genre-help").removeClass("error");
+		}
+	});
+    
 });
